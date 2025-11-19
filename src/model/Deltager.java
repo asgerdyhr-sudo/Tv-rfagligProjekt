feeff806245b8510ef360c85488f1d87097fe777
jonas <jonaso2011@live.dk>
2025-11-19 09:33:30 +0100

    test

--- src/model/Deltager.java
+++ src/model/Deltager.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Deltager {
+    String navn;
+    String adresse;
+    String by;
+    LocalDate ankomstdato;
+    String firmanavn;
+    Status status;
+    int telefon;
+    LocalDate afrejsedato;
+    int firmatelefon;
 }
